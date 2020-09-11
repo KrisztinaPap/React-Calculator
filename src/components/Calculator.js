c77ddbcf5f75ae7d7c1a6689d5fe9a666fa09ece
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-11 16:32:58 -0600

    dds calculateResult function - Thank you to Aaron Barthel for his excellent explanation of state and guidance on fixing my state-bug;

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import '../calculator.css';
 
+
 function Calculator () 
 {
-
     const [ input1, setInput1 ] = useState(0);
-    const [ operation, setOperation ] = useState( '+' );
+    const [ operator, setOperator ] = useState( '+' );
     const [ input2, setInput2 ] = useState(0);
+    const [ result, setResult ] = useState( "" );
+
+
+    const calculateResult = ( event ) => {
+        event.preventDefault();
+        setResult( Number(input1)+Number(input2) );
+    }
 
     return (
-        <form>
+        <form onSubmit={ calculateResult }>
             <label>Input 1:</label>
                 <input 
                     type="number" 
                     name="input1" 
                     onChange={ event => { setInput1( event.target.value ) } }
+                    value={ input1 }
                     />
             <label>Operation:
-                <select onChange={ event => { setOperation( event.target.value ) } }>            
+                <select onChange={ event => { setOperator( event.target.value ) } } value={ operator }>            
                     <option value="+">+</option>
                     <option value="-">-</option>
                     <option value="*">*</option>
@@ -29,8 +37,10 @@ function Calculator ()
                     type="number" 
                     name="input2" 
                     onChange={ event => { setInput2( event.target.value ) } }
+                    value={ input2 }
                 />
             <input id="calculateButton" type="submit" value="Calculate" /> 
+            <p>{ result }</p>
         </form>
     )
 }
